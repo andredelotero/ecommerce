99829b9dbd45f62270173c31aadd885d2664042b
andre gama del otero <andredelotero@gmail.com>
2021-09-18 20:39:07 -0300

    formulario

---
+++ js/main.js
@@ -0,0 +1,15 @@
+class servicio{
+    constructor(id, nombre, descripcion, valor, imagen){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.valor = valor;
+        this.imagen = "../img"+imagen;
+    }
+}
+
+
+/*
+01 wireframe lalala 6500 wireframe.svg
+
+*/
